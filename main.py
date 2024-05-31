501c243b1c5781b9bf1d2c146cf92abb982e58d5
yonatan levi <yhonatanlevi@gmail.com>
2024-05-31 16:53:47 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,4 +8,4 @@ name = st.text_input("Enter your name:")
 color = st.selectbox("Choose your favorite color:", ["Red", "Blue", "Green"])
 
 if st.button("Show Greeting"):
-    st.markdown(f"<h1 style='color: {color};'>Hello, {name}!😃</h1>", unsafe_allow_html=True)
+    st.markdown(f"<h2 style='color: {color};'>Hello, {name}!😃</h2>", unsafe_allow_html=True)
